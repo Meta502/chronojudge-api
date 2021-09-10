3e9edc684282bf3da38988b452fb27e758f61950
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-09-11 03:03:27 +0700

    Support for heroku env

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 
-const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3006;
+const FASTIFY_PORT =
+  Number(process.env.FASTIFY_PORT) || Number(process.env.PORT) || 3006;
 
 app.listen(FASTIFY_PORT);
 
