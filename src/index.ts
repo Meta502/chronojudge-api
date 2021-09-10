b7765e3385e5dbad87f5c0035c13381a69344c8d
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-09-11 03:05:27 +0700

    Listen on all IP addresses

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 const FASTIFY_PORT =
   Number(process.env.FASTIFY_PORT) || Number(process.env.PORT) || 3006;
 
-app.listen(FASTIFY_PORT);
+app.listen(FASTIFY_PORT, "0.0.0.0");
 
 console.log(`🚀  Fastify server running on port ${FASTIFY_PORT}`);
 console.log(`Route index: /`);
